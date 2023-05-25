108e944788b2b7c3ee2fc72df03d64ee66cc5a6a
nemdong <ndw9797@naver.com>
2023-05-25 18:30:10 +0900

    add answer

---
+++ src/main/java/com/example/BackjunsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjunsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/question/B3_2884.java
@@ -0,0 +1,11 @@
+package com.example.question;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B3_2884 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
